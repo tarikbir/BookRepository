b721d9de98cdecf05bc3e86be3e4105af0f2675d
yasinemirkutlu44 <yasinemirkutlu44@gmail.com>
2018-10-10 19:30:19 +0300

    master update merge

--- BookRepository/LoginWindow.xaml.cs
+++ BookRepository/LoginWindow.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BookRepository
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// LoginWindow.xaml etkileşim mantığı
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
-            
         }
     }
 }
